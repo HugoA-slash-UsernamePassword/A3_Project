0fed00867b110e3f8cbc3ea8af8d6b7d80d7c867
H A <hugoa3520@gmail.com>
2020-09-21 02:12:09 +1000

    Movement fixes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,17 +6,35 @@ public class Movement : MonoBehaviour
 {
     public bool test;
     public float speed = 4;
+    public Vector2[] points;
+    public int curPoint;
+    private SimpleTween path;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Clockwise", 0, 2f / speed); //testing
+        //InvokeRepeating("Clockwise", 0, 2f / speed); //testing
+        path = new SimpleTween(transform.position, points[0], Time.time, 1);
+        transform.Rotate(transform.forward, -90);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(transform.position, path.EndPos) > 0.01f)
+        {
+            float timeFraction = (Time.time - path.StartTime) / path.Duration;
+            transform.position = Vector3.Lerp(path.StartPos, path.EndPos, timeFraction);
+        }
+        else
+        {
+            //transform.position = path.EndPos;
+            curPoint++;
+            if (curPoint == points.Length) curPoint = 0;
+            path.Link(points[curPoint]);
+            transform.Rotate(transform.forward, -90);
+        }
         //transform.position = transform.position + (Vector3)dir;
-        transform.position = transform.position + (speed * Time.deltaTime * transform.right);
+        //transform.position = transform.position + (speed * Time.deltaTime * transform.right);
     }
 
     void Clockwise()
---
+++ Assets/Scripts/SimpleTween.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTween
+{
+    public Vector2 StartPos { get; private set; }
+    public Vector2 EndPos { get; private set; }
+    public float StartTime { get; private set; }
+    public float Duration { get; private set; }
+    public SimpleTween(Vector2 StartPos, Vector2 EndPos, float StartTime, float Duration)
+    {
+        this.StartPos = StartPos;
+        this.EndPos = EndPos;
+        this.StartTime = StartTime;
+        this.Duration = Duration;
+    }
+    public void Link(Vector2 newPos)
+    {
+        StartPos = EndPos;
+        EndPos = newPos;
+        StartTime = Time.time;
+    }
+}
