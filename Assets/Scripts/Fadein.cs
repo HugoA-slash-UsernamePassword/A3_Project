319d87c415c2bc4e50f5b53386243fb4543b0f52
H A <hugoa3520@gmail.com>
2020-09-21 20:02:29 +1000

    Level transition and prefab variants.
    SINCE WHEN WAS THAT A THING?????

---
+++ Assets/Scripts/Fadein.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fadein : MonoBehaviour
+{
+    public AudioSource intro;
+    public GameObject player;
+    public GameObject[] ghosts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!intro.isPlaying) //bit of a hacky solution but it works for now
+        {
+            Instantiate(player);
+            foreach (GameObject item in ghosts)
+            {
+                Instantiate(item);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -9,12 +9,19 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem))]
 public class Transition : MonoBehaviour
 {
+    //Animator
+    private int xDir, yDir;
+    public Animator anim;
+    [SerializeField]
+    private testCases testCase;
+    //Particles
     public bool trigger;
     private ParticleSystem ps;
     private ParticleSystem.Particle[] particles = new ParticleSystem.Particle[100];
     void Start()
     {
         ps = GetComponent<ParticleSystem>();
+        InvokeRepeating("test", 3, 3);
     }
 
     // Update is called once per frame
@@ -23,16 +30,84 @@ public class Transition : MonoBehaviour
         if(trigger) { ps.Play(); trigger = false; }
         if(ps.isPlaying)
         {
-            Debug.Log("Triggered");
             ps.GetParticles(particles);
-            for (int i = 0; i < particles.Length; i++)
+            for (int i = 0; i < ps.particleCount; i++)
             {
                 //particles[i].position = Vector3.Lerp(particles[i].position, transform.position, particles[i].velocity.magnitude);
                 if(Vector3.Distance(particles[i].position, transform.position) > 0.1f)
-                particles[i].position = Vector2.Lerp(particles[i].position, transform.position, (1 + (particles[i].remainingLifetime/particles[i].startLifetime * 2)) * Time.deltaTime);                
+                particles[i].position = Vector2.Lerp(particles[i].position, transform.position, (1 + (particles[i].remainingLifetime/particles[i].startLifetime * 2)) * Time.deltaTime);
                 //particles[i].position = Vector3.Lerp(particles[i].position, transform.position, 0.1f * Time.deltaTime);
             }
             ps.SetParticles(particles, particles.Length);
         }
+    }
+    private void LateUpdate()
+    {
+        anim.SetFloat("X", xDir);
+        anim.SetFloat("Y", yDir);
+    }
+    private void Fear(bool toggle)
+    {
+        anim.SetBool("Scared", toggle);
+    }
+    private void Dead(bool toggle)
+    {
+        anim.SetBool("Dead", toggle);
+    }
+    void test()
+    {
+        switch (testCase)
+        {
+            case testCases.left:
+                xDir = -1;
+                yDir = 0;
+                break;
+            case testCases.up:
+                xDir = 0;
+                yDir = 1;
+                break;
+            case testCases.right:
+                xDir = 1;
+                yDir = 0;
+                break;
+            case testCases.down:
+                xDir = 0;
+                yDir = -1;
+                break;
+            case testCases.scared:
+                Fear(true);
+                break;
+            case testCases.notscared:
+                Fear(false);
+                trigger = true;
+                break;
+            case testCases.scaredagain:
+                Fear(true);
+                break;
+            case testCases.dead:
+                Dead(true);
+                break;
+            case testCases.notdead:
+                Dead(false);
+                Fear(false);
+                break;
+            default:
+                break;
+        }
+        testCase++;
+        if (testCase == testCases.leftagain) testCase = testCases.left;
+    }
+    private enum testCases
+    {
+        left,
+        up,
+        right,
+        down,
+        scared,
+        notscared,
+        scaredagain,
+        dead,
+        notdead,
+        leftagain
     }
 }
