da3150affa1ae80defd9bdbdf971e42bda817b82
H A <hugoa3520@gmail.com>
2020-09-21 18:36:58 +1000

    Basic level generator complete

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.Tilemaps;
 
 public class LevelGenerator : MonoBehaviour
 {
-    //public Tile[] palette;
-    //public Tilemap tileMap; //Unfinished; might implement later idk
-    //public Grid grid;
     public GameObject[] levelAssets;
     private int[,] levelMap;
     // Start is called before the first frame update
@@ -41,21 +37,65 @@ public class LevelGenerator : MonoBehaviour
                 Vector2Int pos = new Vector2Int(x, y);
                 bool[] near = new bool[] { IsWall(pos, 0, -1), IsWall(pos, 1, 0), IsWall(pos, 0, 1), IsWall(pos, -1, 0) };
                 int rot = 0;
-                bool isWall = (tileData == 2 || tileData == 4 || tileData == 7); //can simplify later
+                bool hWall = (tileData == 2 || tileData == 4 || tileData == 7); //can simplify later
+                int vOffset = 0;
                 if (tileData == 0) continue;
                 else if (tileData < 5)
                 {
-                    rot = RotateTile(near, tileData, isWall);
-                }
-                //Tilemap (unfinished)
-                //tileMap.SetTile(pos, palette[tileData - 1]); 
-                //Tile tile = tileMap.GetTile(pos) as Tile;
+                    rot = RotateTile(near, tileData, hWall, pos);
+                    if (hWall)
+                    {
+                        if (near[0] && near[2])
+                        {
+                            rot = 90;
+                            vOffset = 180;
+                        }
+                        else if (near[1] && near[3])
+                        {
+                            rot = (!Edge(pos) && tileData == 2 && y != maxY - 2) ? 180 : 0; //bit of a sketchy solution. i'll update here if i figure something out
+                        }
+                        else if ((near[0] || near[2]) && !(near[1] || near[3]))
+                            rot = 90;
+                    }
+                    else
+                    {
+                        if (!Edge(pos) && tileData == 1) tileData = 8;
+                        if(near[0] && near[1] && near[2] && near[3])
+                        {
+                            near = new bool[] { IsWall(pos, 1, -1), IsWall(pos, 1, 1), IsWall(pos, -1, 1), IsWall(pos, -1, -1) };
+                            //if(IsWall(pos))
+                            //for (int i = 0; i < near.Length; i++)
+                            //{
 
-                //Prefab
-                Instantiate(levelAssets[tileData - 1], new Vector3(x - maxX, maxY - y - 1), Quaternion.AngleAxis(rot, Vector3.forward));
-                Instantiate(levelAssets[tileData - 1], new Vector3(x - maxX, y - maxY), Quaternion.AngleAxis(isWall ? rot : 90 - rot, Vector3.forward));
-                Instantiate(levelAssets[tileData - 1], new Vector3(maxX - x - 1, maxY - y - 1), Quaternion.AngleAxis(isWall ? rot : 270 - rot, Vector3.forward));
-                Instantiate(levelAssets[tileData - 1], new Vector3(maxX - x - 1, y - maxY), Quaternion.AngleAxis(isWall ? rot : 180 + rot, Vector3.forward));
+                            //}
+                            if (!near[0])
+                                rot = 90;
+                            else if (!near[1])
+                                rot = 0;
+                            else if (!near[2])
+                                rot = 270;
+                            else if (!near[3])
+                                rot = 180;
+                        }
+                        else if (near[0] && near[1])
+                            rot = 90;
+                        else if (near[1] && near[2])
+                            rot = 0;
+                        else if (near[2] && near[3])
+                            rot = 270;
+                        else if (near[3] && near[0])
+                            rot = 180;
+                        else rot = 90;
+                    }
+                }
+
+                if (y != maxY - 1)
+                {
+                    Instantiate(levelAssets[tileData - 1], new Vector3(x - maxX, maxY - y - 2), Quaternion.AngleAxis(rot, Vector3.forward));
+                    Instantiate(levelAssets[tileData - 1], new Vector3(maxX - x - 1, maxY - y - 2), Quaternion.AngleAxis(hWall ? rot + vOffset : 270 - rot, Vector3.forward));
+                }
+                Instantiate(levelAssets[tileData - 1], new Vector3(x - maxX, y - maxY), Quaternion.AngleAxis(hWall ? 180 + rot + vOffset : 90 - rot, Vector3.forward));
+                Instantiate(levelAssets[tileData - 1], new Vector3(maxX - x - 1, y - maxY), Quaternion.AngleAxis(hWall ? 180 + rot : 180 + rot, Vector3.forward));
             }
         }
     }
@@ -64,12 +104,20 @@ public class LevelGenerator : MonoBehaviour
         Vector2Int p2 = new Vector2Int(p.x + checkX, p.y + checkY);
         if (p2.x < levelMap.GetUpperBound(1) + 1 && p2.x >= 0 && p2.y < levelMap.GetUpperBound(0) + 1 && p2.y >= 0)
         {
-            if (levelMap[p2.y, p2.x] % 5 == 0 || levelMap[p2.y, p2.x] == 6) return false;
+            if ((levelMap[p2.y, p2.x] % 5 == 0 || levelMap[p2.y, p2.x] == 6)) return false;
+            else return true;
         }
         else return false;
-        return true;
     }
-    private int RotateTile(bool[] near, int tileData, bool isWall)
+    private bool Edge(Vector2Int p)
+    {
+        if (p.x == levelMap.GetUpperBound(1) || p.x == 0 || p.y == levelMap.GetUpperBound(0) || p.y == 0)
+        {
+            return true;
+        }
+        else return false;
+    }
+    private int RotateTile(bool[] near, int tileData, bool isWall, Vector2Int pos)
     {
         if (isWall)
         {
